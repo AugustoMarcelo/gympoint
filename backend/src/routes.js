5a03422656d0e6ac66e31502cfa96270b71a64e0
AugustoMarcelo <mrclgst10@gmail.com>
2019-10-21 17:14:05 -0300

    Created students store route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
+import StudentController from './app/controllers/StudentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -9,5 +10,8 @@ const routes = new Router();
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+
+routes.post('/students', StudentController.store);
+// routes.put('/students', StudentControlLer.update);
 
 export default routes;
