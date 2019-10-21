3b4eba960c81560aa74c4d63859a65244bc072ae
AugustoMarcelo <mrclgst10@gmail.com>
2019-10-21 15:08:37 -0300

    importing session controller and authentication middleware

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
-});
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 export default routes;
