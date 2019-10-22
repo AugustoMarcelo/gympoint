05415c2231aac7e895f795439e9dd8cc5f0c58be
AugustoMarcelo <mrclgst10@gmail.com>
2019-10-22 09:25:41 -0300

    Eslint fix

--- backend/src/database/seeds/20191021125954-admin-user.js
+++ backend/src/database/seeds/20191021125954-admin-user.js
@@ -1,18 +1,20 @@
 const bcrypt = require('bcryptjs');
 
 module.exports = {
-  up: (queryInterface) => {
-    return queryInterface.bulkInsert('users',
-    [
-      {
-        name: 'Administrador',
-        email: 'admin@gympoint.com',
-        password_hash: bcrypt.hashSync("123456", 8),
-        created_at: new Date(),
-        updated_at: new Date(),
-      }
-    ],
-    {});
+  up: queryInterface => {
+    return queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          name: 'Administrador',
+          email: 'admin@gympoint.com',
+          password_hash: bcrypt.hashSync('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
   down: () => {},
