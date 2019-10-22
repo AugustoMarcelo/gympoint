a375e3ddbc5d10115197a6c9ce4bd55a59d05baf
AugustoMarcelo <mrclgst10@gmail.com>
2019-10-22 09:23:38 -0300

    Created Plan controller

---
+++ backend/src/app/controllers/PlanController.js
@@ -0,0 +1,65 @@
+import * as Yup from 'yup';
+
+import Plan from '../models/Plan';
+
+class PlanController {
+  async index(request, response) {
+    const plans = await Plan.findAll();
+
+    return response.status(200).json(plans);
+  }
+
+  async store(request, response) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      duration: Yup.number().required(),
+      price: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: 'Validation fails' });
+    }
+
+    const plan = await Plan.create(request.body);
+
+    return response.status(201).json(plan);
+  }
+
+  async update(request, response) {
+    const schema = Yup.object().shape({
+      title: Yup.string(),
+      duration: Yup.number(),
+      price: Yup.number(),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { id } = request.params;
+    const plan = await Plan.findByPk(id);
+
+    if (!plan) {
+      return response.status(400).json({ error: 'Plan not found' });
+    }
+
+    const { title, duration, price } = await plan.update(request.body);
+
+    return response.status(201).json({ id, title, duration, price });
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+    const plan = await Plan.findByPk(id);
+
+    if (!plan) {
+      return response.status(400).json({ error: 'Plan not found' });
+    }
+
+    await plan.destroy();
+
+    return response.status(200).json({ message: 'Plan successfully removed' });
+  }
+}
+
+export default new PlanController();
