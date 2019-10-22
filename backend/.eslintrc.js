673d8969b3d10f2caefc59d3a4ab1087f8d481fb
AugustoMarcelo <mrclgst10@gmail.com>
2019-10-22 08:36:16 -0300

    Trying fix eslint

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -3,10 +3,7 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-    'prettier'
-  ],
+  extends: ['airbnb-base', 'prettier'],
   plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
@@ -21,6 +18,6 @@ module.exports = {
     "class-methods-use-this": "off",
     "no-param-reassign": "off",
     "camelcase": "off",
-    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
   },
 };
