dba1a88d282c00b5f27545aed834853516fc82c4
AugustoMarcelo <mrclgst10@gmail.com>
2019-10-22 09:24:31 -0300

    Registering Plan model

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -2,10 +2,11 @@ import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
 import Student from '../app/models/Student';
+import Plan from '../app/models/Plan';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Student];
+const models = [User, Student, Plan];
 
 class Database {
   constructor() {
@@ -14,9 +15,10 @@ class Database {
 
   init() {
     this.connection = new Sequelize(databaseConfig);
-    models.map(model => model.init(this.connection)).map(model => model.associate && model.associate(this.connection.models));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
 export default new Database();
-
