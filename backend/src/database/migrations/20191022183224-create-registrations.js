88fef5ba43f9c6d87c73cfbc6a7cb23ea6cdccb5
AugustoMarcelo <mrclgst10@gmail.com>
2019-10-23 09:41:47 -0300

    Created migration for registrations

---
+++ backend/src/database/migrations/20191022183224-create-registrations.js
@@ -0,0 +1,50 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('registrations', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      student_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'students', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      plan_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'plans', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      start_date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      end_date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('registrations');
+  },
+};
