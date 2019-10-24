9d3d3c59cfe9a2a397cd08a8b19db7f34184dd2b
AugustoMarcelo <mrclgst10@gmail.com>
2019-10-24 08:07:55 -0300

    Created mail file config

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '51a85093b1c5af',
+    pass: '360dbc40262987',
+  },
+  default: {
+    from: 'Suporte Gympoint <noreply@gympoint.com>',
+  },
+};
