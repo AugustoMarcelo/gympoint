21bf81b90640c0084c300edbc2c113bd3d098076
AugustoMarcelo <mrclgst10@gmail.com>
2019-10-24 09:08:45 -0300

    Created checkins routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,7 @@ import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
 import PlanController from './app/controllers/PlanController';
 import RegistrationController from './app/controllers/RegistrationController';
+import CheckinController from './app/controllers/CheckinController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -30,5 +31,9 @@ routes.post(
 );
 routes.put('/registrations/:id', RegistrationController.update);
 routes.delete('/registrations/:id', RegistrationController.delete);
+
+// Checkin routes
+routes.get('/students/:id/checkins', CheckinController.index);
+routes.post('/students/:id/checkins', CheckinController.store);
 
 export default routes;
