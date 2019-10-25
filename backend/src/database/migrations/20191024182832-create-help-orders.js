f35b1296df20ffe3e342fb8c1ef440378c95b9ca
AugustoMarcelo <mrclgst10@gmail.com>
2019-10-25 08:43:26 -0300

    Created help order migration

---
+++ backend/src/database/migrations/20191024182832-create-help-orders.js
@@ -0,0 +1,37 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('help_orders', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      student_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'students', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      question: {
+        type: Sequelize.STRING(1000),
+        allowNull: false,
+      },
+      answer: Sequelize.TEXT,
+      answer_at: Sequelize.DATE,
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('help_orders');
+  },
+};
