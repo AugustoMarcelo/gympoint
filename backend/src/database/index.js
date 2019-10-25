5bff9e0ed51dd903df50dedec9c5163da6212920
AugustoMarcelo <mrclgst10@gmail.com>
2019-10-25 08:43:47 -0300

    Registering HelpOrder model

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -5,10 +5,11 @@ import Student from '../app/models/Student';
 import Plan from '../app/models/Plan';
 import Registration from '../app/models/Registration';
 import Checkin from '../app/models/Checkin';
+import HelpOrder from '../app/models/HelpOrder';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Student, Plan, Registration, Checkin];
+const models = [User, Student, Plan, Registration, Checkin, HelpOrder];
 
 class Database {
   constructor() {
