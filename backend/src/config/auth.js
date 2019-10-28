e3d814634c48e6b89a2047ad505ab677b92fb428
AugustoMarcelo <mrclgst10@gmail.com>
2019-10-28 15:14:15 -0300

    Movie data to environment variables

--- backend/src/config/auth.js
+++ backend/src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: '2ccc150b9c1c33c3ce116dec447adbd8',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '51a85093b1c5af',
-    pass: '360dbc40262987',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Suporte Gympoint <noreply@gympoint.com>',
