c3071c67318c595dba80901bfa8f4a7cfd87b63e
AugustoMarcelo <mrclgst10@gmail.com>
2019-11-04 20:23:34 -0300

    Added virtual field that returns if the registration is active

--- backend/src/app/models/Registration.js
+++ backend/src/app/models/Registration.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import { parseISO, isAfter } from 'date-fns';
 
 class Registration extends Model {
   static init(sequelize) {
@@ -7,6 +8,12 @@ class Registration extends Model {
         start_date: Sequelize.DATEONLY,
         end_date: Sequelize.DATEONLY,
         price: Sequelize.FLOAT,
+        active: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isAfter(parseISO(this.end_date), new Date());
+          },
+        },
       },
       {
         sequelize,
