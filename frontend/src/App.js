defc1b81bf3182b8e1f390e77496bbeb4c594f34
AugustoMarcelo <mrclgst10@gmail.com>
2019-11-05 20:45:38 -0300

    Imported global style and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+
+import Routes from './routes';
+import history from './services/history';
+
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Gympoint</h1>
-    </div>
+    <Router history={history}>
+      <Routes />
+      <GlobalStyle />
+    </Router>
   );
 }
 
