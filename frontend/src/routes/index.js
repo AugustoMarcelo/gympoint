3a8d15f2cb4fb84096563eb80c5d983dc5bf2c78
AugustoMarcelo <mrclgst10@gmail.com>
2019-11-05 20:47:37 -0300

    Created routes

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
+
+import SignIn from '../pages/SignIn';
+import Dashboard from '../pages/Dashboard';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+    </Switch>
+  );
+}
