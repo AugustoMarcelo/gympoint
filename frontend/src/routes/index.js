6a1ed28562c89e500ef5b50388eae615ed262408
AugustoMarcelo <mrclgst10@gmail.com>
2019-11-09 10:22:58 -0300

    Created students route

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -4,6 +4,7 @@ import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import Dashboard from '../pages/Dashboard';
+import Students from '../pages/Students';
 
 export default function Routes() {
   return (
@@ -11,6 +12,7 @@ export default function Routes() {
       <Route path="/" exact component={SignIn} />
 
       <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/students" component={Students} isPrivate />
     </Switch>
   );
 }
