1a7ad1583e3a8d6138c5d1f135c510624088b9d7
AugustoMarcelo <mrclgst10@gmail.com>
2019-11-11 21:13:46 -0300

    Setting store

---
+++ frontend/src/store/modules/auth/sagas.js
@@ -0,0 +1,47 @@
+import { all, takeLatest, call, put } from 'redux-saga/effects';
+
+import history from '../../../services/history';
+import api from '../../../services/api';
+
+import { signInSuccess, signFailure } from './actions';
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    api.defaults.headers.Authentication = `Bearer ${token}`;
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (err) {
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authentication = `Bearer ${token}`;
+  }
+}
+
+export function signOut() {
+  history.push('/');
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_OUT', signOut),
+]);
