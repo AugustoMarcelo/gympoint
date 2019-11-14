903b29fe7fc95bff1ec1a30e0926b6a08c17b9f8
AugustoMarcelo <mrclgst10@gmail.com>
2019-11-14 08:55:53 -0300

    Removed Add button margin right

--- frontend/src/pages/Students/styles.js
+++ frontend/src/pages/Students/styles.js
@@ -110,36 +110,3 @@ export const EmptyContent = styled.div`
   color: #444;
   font-size: 16px;
 `;
-
-export const Pagination = styled.div`
-  margin-top: 10px;
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-
-  button {
-    border: 1px solid #ee4d64;
-    background: transparent;
-    height: 36px;
-    color: #ee4d64;
-    font-weight: bold;
-    border-radius: 4px;
-    padding: 0 15px;
-    transition: all 0.2s;
-
-    &:first-of-type {
-      margin-right: 10px;
-    }
-
-    &[disabled] {
-      border: 1px solid #bbb;
-      color: #bbb;
-      cursor: not-allowed;
-    }
-
-    &:hover:not([disabled]) {
-      background: #ee4d64;
-      color: #fff;
-    }
-  }
-`;
