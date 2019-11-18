f83ce45dd0334f0ac70575600131150ea0392ca2
AugustoMarcelo <mrclgst10@gmail.com>
2019-11-18 09:04:36 -0300

    Added students create and update routes

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -5,6 +5,7 @@ import Route from './Route';
 import SignIn from '../pages/SignIn';
 import Dashboard from '../pages/Dashboard';
 import Students from '../pages/Students';
+import ManageStudents from '../pages/ManageStudents';
 import Registrations from '../pages/Registrations';
 import Plans from '../pages/Plans';
 import HelpOrders from '../pages/HelpOrders';
@@ -15,6 +16,18 @@ export default function Routes() {
       <Route path="/" exact component={SignIn} />
 
       <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route
+        path="/students/create"
+        exact
+        component={ManageStudents}
+        isPrivate
+      />
+      <Route
+        path="/students/:id/edit"
+        exact
+        component={ManageStudents}
+        isPrivate
+      />
       <Route path="/students" component={Students} isPrivate />
       <Route path="/registrations" component={Registrations} isPrivate />
       <Route path="/plans" component={Plans} isPrivate />
