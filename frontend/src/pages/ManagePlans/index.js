6b242699707ffba7ff693022fc8141e59f114ac6
AugustoMarcelo <mrclgst10@gmail.com>
2019-11-19 16:56:16 -0300

    Created new/edit form to manage plans

---
+++ frontend/src/pages/ManagePlans/index.js
@@ -0,0 +1,139 @@
+import React, { useRef, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { MdCheck, MdKeyboardArrowLeft } from 'react-icons/md';
+import { Form, Input } from '@rocketseat/unform';
+
+import history from '../../services/history';
+import api from '../../services/api';
+
+import { addRequest, updateRequest } from '../../store/modules/plan/actions';
+
+import { Container, Header, Content } from './styles';
+
+export default function ManagePlans({ match }) {
+  const { id } = match.params;
+  const [plan, setPlan] = useState({});
+  const [duration, setDuration] = useState(0);
+  const [total, setTotal] = useState();
+
+  const dispatch = useDispatch();
+  const btnSubmit = useRef();
+
+  function submitForm() {
+    btnSubmit.current.click();
+  }
+
+  function handleSubmit(data, { resetForm }) {
+    if (!id) dispatch(addRequest(data));
+    else dispatch(updateRequest(data, id));
+
+    resetForm();
+    setDuration(0);
+    setTotal(0);
+  }
+
+  function handleBack() {
+    history.push('/plans');
+  }
+
+  function calcTotalPrice(e) {
+    const { value } = e.target;
+    setTotal(value * duration);
+  }
+
+  async function getPlan() {
+    const response = await api.get(`plans/${id}`);
+    setPlan(response.data);
+    setDuration(response.data.duration);
+    setTotal(response.data.duration * response.data.price);
+  }
+
+  useEffect(() => {
+    if (id) {
+      getPlan();
+    }
+  }, [id]);
+
+  return (
+    <Container>
+      <Header>
+        <h2>Cadastro de Planos</h2>
+        <div>
+          <button type="button" className="btn-back" onClick={handleBack}>
+            <MdKeyboardArrowLeft
+              size={20}
+              color="#fff"
+              style={{ marginRight: '5' }}
+            />{' '}
+            Voltar
+          </button>
+          <button type="button" className="btn-save" onClick={submitForm}>
+            <MdCheck size={20} color="#fff" style={{ marginRight: '5' }} />{' '}
+            Salvar
+          </button>
+        </div>
+      </Header>
+      <Content>
+        <Form initialData={plan} onSubmit={handleSubmit}>
+          <Input
+            name="title"
+            placeholder="Descrição do plano"
+            label="Título do plano"
+            autoComplete="off"
+          />
+          <div>
+            <div>
+              <Input
+                name="duration"
+                type="number"
+                label="Duração (em meses)"
+                autoComplete="off"
+                value={duration}
+                onChange={e => setDuration(e.target.value)}
+              />
+            </div>
+            <div>
+              <Input
+                name="price"
+                type="number"
+                onBlur={calcTotalPrice}
+                label="Preço Mensal"
+                autoComplete="off"
+              />
+            </div>
+            <div>
+              <Input
+                name="total"
+                type="number"
+                label="Preço Total"
+                disabled
+                value={total}
+                autoComplete="off"
+              />
+            </div>
+          </div>
+          <button ref={btnSubmit} hidden type="submit">
+            Send
+          </button>
+        </Form>
+      </Content>
+    </Container>
+  );
+}
+
+ManagePlans.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+};
+
+ManagePlans.defaultProps = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: null,
+    }),
+  }),
+};
---
+++ frontend/src/pages/ManagePlans/styles.js
@@ -0,0 +1,96 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  padding: 30px 250px 0px;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  div {
+    display: flex;
+
+    button {
+      color: #fff;
+      text-transform: uppercase;
+      font-weight: bold;
+      border: 0;
+      border-radius: 4px;
+      height: 36px;
+      padding: 0 15px;
+      transition: background 0.2s;
+
+      display: flex;
+      align-items: center;
+
+      &.btn-back {
+        background: #ccc;
+        margin-right: 10px;
+
+        &:hover {
+          background: ${darken(0.1, '#ccc')};
+        }
+      }
+
+      &.btn-save {
+        background: #ee4d64;
+
+        &:hover {
+          background: ${darken(0.1, '#ee4d64')};
+        }
+      }
+    }
+  }
+`;
+
+export const Content = styled.div`
+  margin-top: 20px;
+  background: #fff;
+  padding: 20px;
+  border-radius: 4px;
+
+  form {
+    display: flex;
+    flex-direction: column;
+
+    label {
+      text-transform: uppercase;
+      color: #444;
+      font-weight: bold;
+      margin-bottom: 5px;
+    }
+
+    input {
+      background: transparent;
+      border: 1px solid #ddd;
+      height: 44px;
+      padding: 0 15px;
+      border-radius: 4px;
+      margin-bottom: 15px;
+      color: #444;
+    }
+
+    div {
+      display: flex;
+      justify-content: space-between;
+
+      div {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        margin-right: 20px;
+
+        input[disabled] {
+          background: #f5f5f5;
+        }
+
+        &:last-of-type {
+          margin-right: 0;
+        }
+      }
+    }
+  }
+`;
