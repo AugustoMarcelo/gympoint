5779390150338ea61ffd4d652a677fe31e2fd7ae
AugustoMarcelo <mrclgst10@gmail.com>
2019-11-19 16:57:39 -0300

    Imported plan sagas

--- frontend/src/store/modules/rootSaga.js
+++ frontend/src/store/modules/rootSaga.js
@@ -2,7 +2,8 @@ import { all } from 'redux-saga/effects';
 
 import auth from './auth/sagas';
 import student from './student/sagas';
+import plan from './plan/sagas';
 
 export default function* rootSaga() {
-  return yield all([auth, student]);
+  return yield all([auth, student, plan]);
 }
