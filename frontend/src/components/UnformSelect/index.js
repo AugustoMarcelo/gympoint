476628718648c9a92f1a661c0e3571360dcc4aec
AugustoMarcelo <mrclgst10@gmail.com>
2019-11-26 21:17:38 -0300

    Implemented Select from react-select to use with unform

---
+++ frontend/src/components/UnformSelect/index.js
@@ -0,0 +1,79 @@
+import React, { useRef, useEffect } from 'react';
+import Select from 'react-select';
+import PropTypes from 'prop-types';
+
+import { useField } from '@rocketseat/unform';
+
+export default function UnformSelect({
+  name,
+  label,
+  options,
+  multiple,
+  ...rest
+}) {
+  const ref = useRef(null);
+  const { fieldName, registerField, defaultValue, error } = useField(name);
+
+  function parseSelectValue(selectRef) {
+    const selectValue = selectRef.state.value;
+    if (!multiple) {
+      return selectValue ? selectValue.id : '';
+    }
+
+    return selectValue ? selectValue.map(option => option.id) : [];
+  }
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: ref.current,
+      path: 'state.value',
+      parseValue: parseSelectValue,
+      clearValue: selectRef => {
+        selectRef.select.clearValue();
+      },
+    });
+  }, [ref.current, fieldName]); // eslint-disable-line
+
+  function getDefaultValue() {
+    if (!defaultValue) return null;
+
+    if (!multiple) {
+      return options.find(option => option.id === defaultValue);
+    }
+
+    return options.filter(option => defaultValue.includes(option.id));
+  }
+
+  return (
+    <>
+      {label && <label htmlFor={fieldName}>{label}</label>}
+
+      <Select
+        name={fieldName}
+        aria-label={fieldName}
+        options={options}
+        isMulti={multiple}
+        defaultValue={getDefaultValue()}
+        ref={ref}
+        getOptionValue={option => option.id}
+        getOptionLabel={option => option.title}
+        {...rest}
+      />
+
+      {error && <span>{error}</span>}
+    </>
+  );
+}
+
+UnformSelect.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  options: PropTypes.arrayOf,
+  multiple: PropTypes.bool,
+};
+
+UnformSelect.defaultPropts = {
+  options: [],
+  multiple: false,
+};
