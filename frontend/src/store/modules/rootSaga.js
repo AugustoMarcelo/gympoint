717aae29c4927649c5c3241ca08d2bf252e298ec
AugustoMarcelo <mrclgst10@gmail.com>
2019-11-27 21:23:29 -0300

    Imported help order sagas

--- frontend/src/store/modules/rootSaga.js
+++ frontend/src/store/modules/rootSaga.js
@@ -3,7 +3,8 @@ import { all } from 'redux-saga/effects';
 import auth from './auth/sagas';
 import student from './student/sagas';
 import plan from './plan/sagas';
+import helporder from './helporder/sagas';
 
 export default function* rootSaga() {
-  return yield all([auth, student, plan]);
+  return yield all([auth, student, plan, helporder]);
 }
