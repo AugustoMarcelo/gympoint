4e07f06c9777add98c010c8dac4172b85fbed5a8
AugustoMarcelo <mrclgst10@gmail.com>
2019-12-19 16:47:39 -0300

    Created checkins route

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,11 +1,8 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-import Login from '~/pages/Login';
-import HelpOrder from '~/pages/HelpOrder';
-import HelpOrders from '~/pages/HelpOrders';
-import ManageHelpOrder from '~/pages/ManageHelpOrder';
-import Checkins from '~/pages/Checkins';
+import Login from './pages/Login';
+import Checkins from './pages/Checkins';
 
 export default createAppContainer(
   createSwitchNavigator({
@@ -13,16 +10,11 @@ export default createAppContainer(
     Main: createBottomTabNavigator(
       {
         Checkins,
-        HelpOrders: createSwitchNavigator({
-          HelpOrders,
-          HelpOrder,
-          ManageHelpOrder,
-        }),
       },
       {
         tabBarOptions: {
           keyboardHidesTabBar: true,
-          activeTintColor: '#EE4E62',
+          activeTintColor: '#ee4e62',
           inactiveTintColor: '#999999',
           style: {
             backgroundColor: '#fff',
