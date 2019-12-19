734d1383dea8dd9a2e807b6361f0bde84855576b
AugustoMarcelo <mrclgst10@gmail.com>
2019-12-19 16:47:07 -0300

    Imported styled components from styles

--- mobile/src/pages/Login/index.js
+++ mobile/src/pages/Login/index.js
@@ -1,74 +1,31 @@
-import React from 'react';
-import {
-  KeyboardAvoidingView,
-  StyleSheet,
-  Platform,
-  Text,
-  TextInput,
-  Image,
-  TouchableOpacity,
-} from 'react-native';
+import React, { useState } from 'react';
+import { Platform, Image } from 'react-native';
 
-import logo from '~/assets/logo.png';
+import logo from '../../assets/logo.png';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 30,
-  },
-  title: {
-    textTransform: 'uppercase',
-    fontWeight: 'bold',
-    color: '#EE4E62',
-    fontSize: 24,
-    marginTop: 10,
-  },
-  input: {
-    height: 46,
-    alignSelf: 'stretch',
-    backgroundColor: '#fff',
-    borderWidth: 1,
-    borderColor: '#ddd',
-    borderRadius: 4,
-    marginTop: 20,
-    paddingHorizontal: 15,
-  },
-  button: {
-    height: 46,
-    backgroundColor: '#EE4E62',
-    alignSelf: 'stretch',
-    borderRadius: 4,
-    marginTop: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: '#fff',
-    fontWeight: 'bold',
-  },
-});
+import { Container, Title, Input, Button, ButtonText } from './styles';
 
 export default function Login({ navigation }) {
+  const [id, setId] = useState('');
+
+  async function handleLogin() {
+    navigation.navigate('Main', { id });
+  }
+
   return (
-    <KeyboardAvoidingView
-      behavior="padding"
-      enabled={Platform.OS === 'ios'}
-      style={styles.container}
-    >
+    <Container behavior="padding" enabled={Platform.OS === 'ios'}>
       <Image source={logo} />
-      <Text style={styles.title}>GYMPOINT</Text>
-      <TextInput
+      <Title>GYMPOINT</Title>
+      <Input
         autoCapitalize="none"
         autoCorrect={false}
         placeholder="Informe seu ID de cadastro"
-        style={styles.input}
+        value={id}
+        onChangeText={setId}
       />
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Entrar no sistema</Text>
-      </TouchableOpacity>
-    </KeyboardAvoidingView>
+      <Button onPress={handleLogin}>
+        <ButtonText>Entrar no sistema</ButtonText>
+      </Button>
+    </Container>
   );
 }
