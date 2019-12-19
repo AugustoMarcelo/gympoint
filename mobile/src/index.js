03377dddff1794cee839b19064e617c2e00830a4
AugustoMarcelo <mrclgst10@gmail.com>
2019-12-19 16:48:03 -0300

    Removed store configs

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 
-import '~/config/ReactotronConfig';
-
-import { Provider } from 'react-redux';
-import store from './store';
-
-import Routes from '~/routes';
+import Routes from './routes';
 
 const App = () => (
-  <Provider store={store}>
+  <>
     <StatusBar barStyle="dark-content" backgroundColor="#fff" />
     <Routes />
-  </Provider>
+  </>
 );
 
 export default App;
