8d694a0ab83241370194a67830a304fca9c2a6f1
AugustoMarcelo <mrclgst10@gmail.com>
2020-01-04 11:31:28 -0300

    Added rule to ignore console use

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+
+  globals: {
+    __DEV__: 'readonly'
+  },
+
+  rules: {
+    'no-console': ["error", { allow: ['tron'] }],
+  }
 };
