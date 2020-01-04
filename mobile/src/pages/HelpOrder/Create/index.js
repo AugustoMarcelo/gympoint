4bf49c475990269d7a5ad62d10846f423a1025f7
AugustoMarcelo <mrclgst10@gmail.com>
2020-01-04 11:35:56 -0300

    Implemented help order register

--- mobile/src/pages/HelpOrder/Create/index.js
+++ mobile/src/pages/HelpOrder/Create/index.js
@@ -1,11 +1,40 @@
 import React, { useState } from 'react';
+import { ToastAndroid } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
-import Header from '../../../components/Header';
+import api from '../../../services/api';
 
 import { Content, Input, Button, ButtonText } from './styles';
 
 export default function Create() {
   const [question, setQuestion] = useState('');
+
+  async function handleHelpOrder() {
+    const id = await AsyncStorage.getItem('id');
+
+    if (question) {
+      const { status } = await api.post(`/students/${id}/help-orders`, {
+        question,
+      });
+
+      ToastAndroid.show(getMessageStatus(status), ToastAndroid.LONG);
+    } else {
+      ToastAndroid.show('Descreva seu pedido', ToastAndroid.LONG);
+    }
+  }
+
+  function getMessageStatus(status) {
+    const messages = {
+      201: () => {
+        setQuestion('');
+        return 'Pedido registrado com sucesso';
+      },
+      404: () => 'Aluno não encontrado. Tente novamente mais tarde',
+      default: () => 'Não foi possível registrar seu pedido.',
+    };
+
+    return (messages[status] || messages.default)();
+  }
 
   return (
     <>
@@ -19,7 +48,7 @@ export default function Create() {
           multiline
           textAlignVertical="top"
         />
-        <Button>
+        <Button onPress={handleHelpOrder}>
           <ButtonText>Enviar pedido</ButtonText>
         </Button>
       </Content>
