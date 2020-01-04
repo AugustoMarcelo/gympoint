aaaa22fb130f6e4cf97987e7a0a43e81fa8fe5ae
AugustoMarcelo <mrclgst10@gmail.com>
2020-01-04 20:41:30 -0300

    Imported registration sagas

--- frontend/src/store/modules/rootSaga.js
+++ frontend/src/store/modules/rootSaga.js
@@ -4,7 +4,8 @@ import auth from './auth/sagas';
 import student from './student/sagas';
 import plan from './plan/sagas';
 import helporder from './helporder/sagas';
+import registration from './registration/sagas';
 
 export default function* rootSaga() {
-  return yield all([auth, student, plan, helporder]);
+  return yield all([auth, student, plan, helporder, registration]);
 }
