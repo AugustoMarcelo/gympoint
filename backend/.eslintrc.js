42e6ef0da4835216fefa76ec9301d00192fdd181
AugustoMarcelo <mrclgst10@gmail.com>
2020-01-05 17:44:14 -0300

    Added rule to ignore default exports

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   },
   rules: {
     "prettier/prettier": ["error", { "endOfLine": "auto" }],
+    'import/prefer-default-export': 'off',
     "class-methods-use-this": "off",
     "no-param-reassign": "off",
     "camelcase": "off",
